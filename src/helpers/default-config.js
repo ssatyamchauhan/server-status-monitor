f9185081ae9da9cd2bdccf8270a8cfdb0f9ae63c
Satyam Singh <officialsatyam1010@gmail.com>
2022-08-09 17:15:18 +0530

    inital commit

---
+++ src/helpers/default-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: 'Server Status',
+  theme: 'default.css',
+  path: '/status',
+  socketPath: '/socket.io',
+  spans: [
+    {
+      interval: 1,
+      retention: 60,
+    },
+    {
+      interval: 5,
+      retention: 60,
+    },
+    {
+      interval: 15,
+      retention: 60,
+    },
+  ],
+  port: null,
+  websocket: null,
+  iframe: false,
+  apiUrl: false,
+  chartVisibility: {
+    cpu: true,
+    mem: true,
+    load: true,
+    heap: true,
+    eventLoop: true,
+    responseTime: true,
+    rps: true,
+    statusCodes: true,
+  },
+  ignoreStartsWith: '/admin',
+  healthChecks: [],
+};
